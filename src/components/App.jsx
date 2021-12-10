345ed763783295f74b31601e4677562cc32a0639
ArturdaSilvaRezende <artursrezende@gmail.com>
2021-12-10 17:12:08 -0300

    c-pages-about finished is responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,30 @@ import React from "react";
 import "./global-style.css";
 
 //components
-import Header from "./Header/Header";
-import Hero from "./Hero/Hero";
-import Main from "./Main/Main";
-import Footer from "./Footer/Footer";
+import Header from "./Home/Header/Header";
+import Home from "./Home/Home";
+import About from "./Pages/About/About";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Contact from "./Pages/Contact/Contact";
+import Services from "./Pages/Services/Services";
+
+//react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Hero />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="Home" element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="Portfolio" element={<Portfolio />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="Services" element={<Services />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/components/Pages/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = (props) => {
+  return (
+    <section>
+      <h1>Page Services</h1>
+    </section>
+  );
+};
+
+export default About;
